10e8f3700d04b278373e13df23fdcd0751f1d66b
citizenfish <dave@phased.co.uk>
2019-03-15 09:01:57 +0000

    Updated views and leaderboard report

---
+++ server_config/reports/leaderboard.sql
@@ -0,0 +1,15 @@
+--This report pulls the data required to create the leaderboard
+
+DELETE FROM ng_adventuresyndicate.reports WHERE report_name = 'leaderboard';
+
+
+INSERT INTO ng_adventuresyndicate.reports(report_name, report_query, report_attributes)
+VALUES(
+'leaderboard',
+$SQL$
+
+    SELECT  json_agg(json_build_object('position', attributes->>'position', 'org_name', attributes->>'org_name', 'distance_miles', attributes->>'distance_miles', 'distance_km',attributes->>'distance_km', 'date', to_char((attributes->>'dateTime')::TIMESTAMP, 'DD/MM/YYYY HH24:MM'))) FROM ng_adventuresyndicate.tracker_points
+
+$SQL$,
+    json_build_object('user_type', json_build_array('registered','administrator','public'))
+);
--- tests/add_mileage_record.sql
+++ tests/add_mileage_record.sql
@@ -3,8 +3,10 @@ $$
 DECLARE
     ret_var JSON;
 BEGIN
+    DELETE FROM features where feature_id >= 822926;
+
     SELECT ng_rest.add_feature('ng_adventuresyndicate',
-						  json_build_object('attributes', json_build_object('feature_type', 'distance_record', 'org_name', 'school foo', 'distance', 1000))) into ret_var;
+						  json_build_object('attributes', json_build_object('feature_type', 'distance_record', 'org_name', 'school foo', 'distance', 1000, 'org_id', 'ORG01', 'org_rider', 'Dave Barter'))) into ret_var;
 
     RAISE NOTICE '%',ret_var;
 END;
