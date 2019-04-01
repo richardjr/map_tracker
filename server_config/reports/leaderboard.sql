6bfaee14b71b74d80fcb27ba3f55419fb639e9e0
Dave Barter <dave@phased.co.uk>
2019-04-01 18:36:20 +0100

    Added categories

--- server_config/reports/leaderboard.sql
+++ server_config/reports/leaderboard.sql
@@ -8,7 +8,18 @@ VALUES(
 'leaderboard',
 $SQL$
 
-    SELECT  json_agg(json_build_object('position', attributes->>'position', 'org_name', attributes->>'org_name', 'distance_miles', attributes->>'distance_miles', 'distance_km',attributes->>'distance_km', 'date', to_char((attributes->>'dateTime')::TIMESTAMP, 'DD/MM/YYYY HH24:MM'))) FROM ng_adventuresyndicate.tracker_points
+    SELECT  json_agg(
+    	json_build_object(	'position', 		attributes->>'position',
+    						'org_name', 		attributes->>'org_name',
+    						'distance_miles', 	attributes->>'distance_miles',
+    						'distance_km',		attributes->>'distance_km',
+    						'date', 			to_char((attributes->>'dateTime')::TIMESTAMP, 'DD/MM/YYYY HH24:MM'),
+    						'category', 		COALESCE(attributes->>'category', 'school')
+
+    					))
+
+   FROM ng_adventuresyndicate.tracker_points
+   WHERE WHERE_CLAUSE
 
 $SQL$,
     json_build_object('user_type', json_build_array('registered','administrator','public'))
